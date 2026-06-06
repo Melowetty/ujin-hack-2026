a06b488e17e7aab7631ef6b22f506d480bb71ca6
Danil <dekunakbaev@outlook.com>
2026-06-06 18:10:04 +0500

    fix controllers

--- src/main/kotlin/team/mcqueen/smartdisplay/controller/ComplexesController.kt
+++ src/main/kotlin/team/mcqueen/smartdisplay/controller/ComplexesController.kt
@@ -1,10 +1,12 @@
 package team.mcqueen.smartdisplay.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
 import team.mcqueen.smartdisplay.generated.api.ComplexesApi
 import team.mcqueen.smartdisplay.generated.model.Complex
 import team.mcqueen.smartdisplay.generated.model.House
 
+@RestController
 class ComplexesController : ComplexesApi {
     override fun getComplexes(): ResponseEntity<List<Complex>> {
         return ResponseEntity.ok(listOf(
--- src/main/kotlin/team/mcqueen/smartdisplay/controller/EmergencyController.kt
+++ src/main/kotlin/team/mcqueen/smartdisplay/controller/EmergencyController.kt
@@ -1,12 +1,14 @@
 package team.mcqueen.smartdisplay.controller
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
 import team.mcqueen.smartdisplay.generated.api.EmergencyApi
 import team.mcqueen.smartdisplay.generated.model.Emergency
 import team.mcqueen.smartdisplay.generated.model.EmergencyInput
 import team.mcqueen.smartdisplay.generated.model.TargetType
 import java.time.OffsetDateTime
 
+@RestController
 class EmergencyController : EmergencyApi {
     override fun createEmergency(emergencyInput: EmergencyInput): ResponseEntity<Emergency> {
         return ResponseEntity.ok(Emergency(
